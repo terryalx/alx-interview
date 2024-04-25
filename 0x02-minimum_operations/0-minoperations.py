8b440551ecb1bd2b689d494c3840caf382128fa3
Efe-Terry <info.techbro.set@gmail.com>
2024-04-25 21:38:45 +0100

    0x02-minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,19 +5,18 @@ Minimum operations to archieve a certain number of characters.
 
 
 def minOperations(n):
-    """Returns the minimum operations to archieve n characters."""
+    """Returns the minimum operations required to achieve n characters."""
     if n <= 1:
         return 0
-    else:
-        for i in range(2, n + 1):
-            if n % i == 0:
-                return minOperations(n // i) + i
-        return n
-
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return minOperations(n // i) + i
+        i += 1
+    return n
 
 if __name__ == "__main__":
     test_cases = [4, 12]
     for n in test_cases:
-    	print(
-    	    "Min # of operations to reach {} char: {}".format(n, minOperations(n))
-    	        )
+        print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
