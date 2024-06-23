0a9d66a684fd963648d01f18a4bc8e7d76063eac
Efe-Terry <info.techbro.set@gmail.com>
2024-06-23 08:58:43 +0100

    0x0A-primegame

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Prime Game"""
+
+
+def isWinner(x, nums):
+    """function that checks for the winner."""
+    if not nums or x < 1:
+        return None
+    max_num = max(nums)
+
+    filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            filter[j] = False
+    filter[0] = filter[1] = False
+    y = 0
+    for i in range(len(filter)):
+        if filter[i]:
+            y += 1
+        filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Fola"
+    return "Efe"
+
+
+if __name__ == "__main__":
+    assert isWinner(5, [2, 5, 1, 4, 3]) == "Efe", "Expected Efe"
